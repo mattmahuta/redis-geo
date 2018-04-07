f444bf2ddb6e110ff9818da3be04379aeb5f2a8a
Mahuta-Digital <matt@mahuta.io>
2018-04-07 18:03:22 +1200

    More debug statements

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,7 @@ source ../deploy-envs.sh
 export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
 # Build process
+echo "Building docker image $IMAGE_NAME"
 docker build -t $IMAGE_NAME ../
 docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
+echo "Docker image built"
