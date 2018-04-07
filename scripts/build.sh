c619ec9206179f9ab115d758fef6430d4eccb233
Mahuta-Digital <matt@mahuta.io>
2018-04-07 18:04:36 +1200

    More and more debug starements

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,4 +8,4 @@ export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazo
 echo "Building docker image $IMAGE_NAME"
 docker build -t $IMAGE_NAME ../
 docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
-echo "Docker image built"
+echo "Docker image built with tag: $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION"
